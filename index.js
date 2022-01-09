73a54d6f4847a83fd45a0ce0492261a89926f534
alaamerhebb <alaamerhebb@gmail.com>
2022-01-10 00:28:16 +0200

    step 9

--- index.js
+++ index.js
@@ -68,7 +68,18 @@ app.get("/movies/read", (req, res) => {
 
 app.get("/movies/update", (req, res) => {});
 
-app.get("/movies/delete", (req, res) => {});
+app.get("/movies/delete/:ID", (req, res) => {
+  if (Number(req.params.ID) >= 0 && req.params.ID < movies.length) {
+    movies.splice(parseInt(req.params.ID), 1);
+    res.send(movies);
+  } else {
+    res.status(404).send({
+      status: 404,
+      error: true,
+      message: `the movie ${req.params.ID} does not exist`,
+    });
+  }
+});
 
 app.get("/movies/read/:content", (req, res) => {
     if (req.params.content == "by-date") {
