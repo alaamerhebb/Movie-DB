acd3b4bc86513e930d1e252ab39d7075b1c9463d
alaamerhebb <alaamerhebb@gmail.com>
2022-01-09 21:09:44 +0200

    step5

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 
 app.get("/", (req, res) => {
   res.send("ok.");
@@ -30,5 +37,15 @@ app.get("/hello/:userID", (req, res) => {
       });
     }
   });
+  app.get("/movies/create", (req, res) => {});
+
+app.get("/movies/read", (req, res) => {
+  res.status(200).send({ status: 200, data: movies });
+});
+
+app.get("/movies/update", (req, res) => {});
+
+app.get("/movies/delete", (req, res) => {});
+
 
 app.listen(port, console.log(`Connected to ${port}`));
